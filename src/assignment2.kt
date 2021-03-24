2c105a461fe4a8a4178285efee38dbf878050e1a
Jane-Nyambura <janekamaunyambura@gmail.com>
2021-03-24 16:48:12 +0300

    first commit

---
+++ src/assignment2.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val name:String=("gillyOluch")
+    println(name)
+    var age: Byte=20
+    println(age)
+    var phoneNumber :String="0786183897"
+    println(phoneNumber)
+    var weight:Float=60.56.toFloat()
+    println(weight)
+    var citizen:String="kenyan"
+    println(citizen)
+}
